6340e1019f037390e468efac7fac7abd3591b333
simag <maggsimo@gmail.com>
2010-07-04 11:22:11 +0200

    remove unused include

--- src/KscInterface/ksc.cpp
+++ src/KscInterface/ksc.cpp
@@ -12,6 +12,14 @@ ksc::ksc(QWidget *parent) :
     QMainWindow(parent)
 {
     setupUi(this);
+
+    NetworkConnectionManager * net;
+    net = new NetworkConnectionManager();
+
+    if( net->isNetworkAvailable() )
+        kDebug()<<"si";
+    else
+        kDebug()<<"no";
 }
 
 ksc::~ksc()
